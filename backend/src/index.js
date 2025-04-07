d6af44d6768fdf4360df8ee8cf2735e8c9becf7d
DhruvPatel284 <dhruv156328@gmail.com>
2025-04-07 10:45:59 +0530

    CORS updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,12 +19,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
