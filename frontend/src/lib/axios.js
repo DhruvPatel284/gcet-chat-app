7a40d5057c7a3edf40033dfc00a726f9e52aea3a
DhruvPatel284 <dhruv156328@gmail.com>
2025-08-02 18:47:49 +0530

    some new updates

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 console.log("backend url : ",backendUrl)
 export const axiosInstance = axios.create({
-  baseURL: (backendUrl ? backendUrl + "/api" : "http://localhost:5001/api"),
+  baseURL: (backendUrl ? backendUrl : "http://localhost:5001/api"),
   withCredentials: true,
 });
 
